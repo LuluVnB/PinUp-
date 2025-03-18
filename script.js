0bf8f520c90dbed6dfa9ec9ad0a66f0273cac613
Nibblets2000 <tsimeng@hotmail.com>
2025-03-17 19:53:36 -0700

    script/button function setup

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("hello world justin was here");
+
+//Profile section
+
+
+
+//Button functionality
+searchBtn = document.getElementsByClassName("sidebar-button");
+taskBtn = document.getElementsByClassName("sidebar-button");
+notifBtn = document.getElementsByClassName("sidebar-button");
+mailBtn = document.getElementsByClassName("sidebar-button");
+settingBtn = document.getElementsByClassName("sidebar-button");
+
+searchBtn.onclick = function(){
+
+}
+
+taskBtn.onclick = function(){
+    
+}
+
+notifBtn.onclick = function(){
+    
+}
+
+mailBtn.onclick = function(){
+    
+}
+
+settinghBtn.onclick = function(){
+    
+}
